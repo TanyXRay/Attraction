70a1789b9388c3c6423c06129d0fbaeeda6c8f20
Tatyana Chernyadieva <tany.taray@gmail.com>
2022-09-09 18:29:20 +0400

    add files

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Решение задачи №1 по теме: "Коллекции Queue".
+ * Реализация симулятора аттракциона.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Queue<Person> personQueue = new ArrayDeque<>();
+        personQueue.offer(generateClients().get(0));
+        personQueue.offer(generateClients().get(1));
+        personQueue.offer(generateClients().get(2));
+        personQueue.offer(generateClients().get(3));
+        personQueue.offer(generateClients().get(4));
+
+        printQueue(personQueue);
+    }
+
+    /**
+     * Метод по созданию списка.
+     *
+     * @return List - список из людей.
+     */
+    public static List<Person> generateClients() {
+        List<Person> listOfPerson = new ArrayList<>();
+        listOfPerson.add(new Person("Julia", "Holland", 6));
+        listOfPerson.add(new Person("Sarah", "Jacks", 3));
+        listOfPerson.add(new Person("Alex", "Attwood", 5));
+        listOfPerson.add(new Person("John", "Cantwell", 10));
+        listOfPerson.add(new Person("Bruce", "Farrel", 8));
+        return listOfPerson;
+    }
+
+    /**
+     * Метод выводит на экран по очереди людей, у которых есть билеты на аттракцион.
+     *
+     * @param personQueue очередь из людей.
+     */
+    public static void printQueue(Queue<Person> personQueue) {
+        while (!personQueue.isEmpty()) {
+            int countOfTickets;
+            for (Person p : personQueue) {
+                countOfTickets = p.getNumberOfTickets() - 1;
+                if (!(countOfTickets == 0)) {
+                    System.out.println(p + " прокатил(ась)ся на аттракционе!");
+                    p.setNumberOfTickets(countOfTickets);
+                } else {
+                    System.out.println("У " + p.getName() + " " + p.getSurname() + " закончились билеты!(выбывает из очереди)");
+                    personQueue.remove(p);
+                }
+            }
+        }
+    }
+}
+
---
+++ src/Person.java
@@ -0,0 +1,35 @@
+/**
+ * Данный класс описывает человека, который посетил парк аттракционов.
+ */
+public class Person {
+    private String name;
+    private String surname;
+    private int numberOfTickets;
+
+    public Person(String name, String surname, int numberOfTickets) {
+        this.name = name;
+        this.surname = surname;
+        this.numberOfTickets = numberOfTickets;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getNumberOfTickets() {
+        return numberOfTickets;
+    }
+
+    public void setNumberOfTickets(int numberOfTickets) {
+        this.numberOfTickets = numberOfTickets;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname;
+    }
+}
